b5e7f2ad3e9f1fa275588dde3f2bdd8b78a9d319
Abdul Nur <abduljabar@Abduls-MacBook-Pro.local>
2023-02-08 19:41:05 -0600

    added a public folder.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = 3001 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
