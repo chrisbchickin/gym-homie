1a33ebed54edbc0a92bb7d35bef97fd6ad1051a0
Chrisbchicken <chrsbnjmn@gmail.com>
2023-02-09 00:51:04 -0600

    routes update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
