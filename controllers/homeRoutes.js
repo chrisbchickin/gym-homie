24eade6dd3307f64fa44e4345170070ce4a26f9e
Chrisbchicken <chrsbnjmn@gmail.com>
2023-02-10 12:58:05 -0600

    created profile page/ created logout button/functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,28 @@
 const router = require('express').Router();
+const { User } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
         res.render('homepage');
     } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] }
+          });
+      
+          const user = userData.get({ plain: true });
+      
+          res.render('profile', {
+            ...user,
+            logged_in: true
+          });
+    } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 })
