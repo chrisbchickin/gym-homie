9b77ead8468f85845dda5edf8001b50d6799416e
JoshuaHamann <joshwashku@yahoo.com>
2023-02-13 16:38:04 -0600

    post method for exercise

--- controllers/api/exerciseRoutes.js
+++ controllers/api/exerciseRoutes.js
@@ -10,7 +10,12 @@ router.get('/:category_id', async (req,res) => {
     }
 });
 router.post('/', async (req, res) => {
+   try{
     const newExercise = await Exercise.create(req.body);
+    res.status(200).json(newExercise);
+   } catch (err) {
+    res.status(500).json(err);
+   };
 });
 
 module.exports = router;
