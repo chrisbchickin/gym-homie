415d956374180e186f2d43a0f28c1168b1f7c643
JoshuaHamann <joshwashku@yahoo.com>
2023-02-13 16:09:56 -0600

    added a route to grab all info b

--- controllers/api/exerciseRoutes.js
+++ controllers/api/exerciseRoutes.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const { Exercise } = require('../../models');
 
+router.get('/:category_id', async (req,res) => {
+    try{
+        const allExerciseByCategory = await Exercise.findAll({where: {category_id: req.params.category_id}});
+        res.status(200).json(allExerciseByCategory);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 router.post('/', async (req, res) => {
     const newExercise = await Exercise.create(req.body);
 });
